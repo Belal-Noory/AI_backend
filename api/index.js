f0918733304382e8126be4167a7ea19f280d3dd7
Belal Noory <belalnoory2@gmail.com>
2023-03-20 00:16:32 +0430

    thired commit

--- api/index.js
+++ api/index.js
@@ -1,18 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import userRoute from "./routes/userRoute.js";
-import chatRoute from "./routes/chatRoute.js";
-import db from "./db/db.js";
-import { errorHandler, notFound } from "./middleware/error.js";
+import userRoute from "../routes/userRoute.js";
+import chatRoute from "../routes/chatRoute.js";
+import db from "../db/db.js";
+import { errorHandler, notFound } from "../middleware/error.js";
 
 dotenv.config();
 const app = express();
 let port = process.env.PORT || 5000;
 
-app.use(cors({
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 db;
 
@@ -29,5 +27,4 @@ app.listen(port, () => {
   console.log(`App is running on the Port ${port}`);
 });
 
-module.exports = app
-
+module.exports = app;
